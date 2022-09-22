56fcfdd06969078baabc3b7dde8bc2bd9054e954
dayeonYou <0620dadada@suwon.ac.kr>
2022-09-23 03:22:22 +0900

    main2

---
+++ app/src/main/java/com/example/main2/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.main2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Btn1.class);
+                startActivity(intent);
+            }
+        });
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Btn2.class);
+                startActivity(intent);
+            }
+        });
+        Button btn3 = (Button) findViewById(R.id.btn3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Btn3.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
