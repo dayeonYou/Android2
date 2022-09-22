f3b800a6af8c918eff3cbd7d3b2bd231cdde0717
dayeonYou <0620dadada@suwon.ac.kr>
2022-09-23 04:08:34 +0900

    main2

--- app/src/main/java/com/example/main2/MainActivity.java
+++ app/src/main/java/com/example/main2/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.main2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,5 +47,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        if(false == isConnected()) {
+            Toast.makeText(this,"not connected",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        switch (getNetworkType()){
+            case ConnectivityManager.TYPE_WIFI:
+                Toast.makeText(this,"connected to WI-FI",Toast.LENGTH_SHORT).show();
+                HttpMgrTread httpThread = new HttpMgrTread();
+                httpThread.start();
+                break;
+            case ConnectivityManager.TYPE_MOBILE:
+                Toast.makeText(this,"connected to MOBILE",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+    private boolean isConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetWork = cm.getActiveNetworkInfo();
+        boolean isConnected = (activeNetWork != null) && (activeNetWork.isConnectedOrConnecting());
+        return isConnected;
+    }
+    private int getNetworkType(){
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork.getType();
     }
 }
